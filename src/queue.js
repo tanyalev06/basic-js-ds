ce0dd9ca14a1f30dac6e0034ef7eebc69ad60d6c
Tatyana Levchenko <grib.tanya06@mail.ru>
2025-11-11 00:21:59 +0300

    feat: add queue3

--- src/queue.js
+++ src/queue.js
@@ -1,33 +1,40 @@
-const { NotImplementedError } = require('../lib/errors');
-// const { ListNode } = require('../extensions/list-node.js');
-
-/**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- * queue.getUnderlyingList() // returns { value: 3, next: null }
- */
+const { ListNode } = require('../extensions/list-node.js');
 class Queue {
-  getUnderlyingList() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.head = null; // первый элемент очереди
+    this.tail = null; // последний элемент очереди
   }
 
-  enqueue(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    const newNode = new ListNode(value);
+
+    if (!this.head) {
+      // если очередь пуста, head и tail указывают на новый узел
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // добавляем в конец и обновляем tail
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
   }
 
   dequeue() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!this.head) return null; // если очередь пустая
+
+    const value = this.head.value; // сохраняем значение
+    this.head = this.head.next; // сдвигаем голову
+
+    if (!this.head) this.tail = null; // если очередь стала пустой, tail обнуляем
+
+    return value;
   }
 }
+
 
 module.exports = {
   Queue
